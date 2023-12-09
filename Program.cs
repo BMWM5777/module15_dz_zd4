408326404ad08bee4efcfd912c2a58307d224c4f
mrdil <mrdil@BMW>
2023-12-09 22:22:16 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module15_dz_zd4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Type listType = typeof(List<>);
+            Type genericArgument = typeof(int);
+
+            Type constructedListType = listType.MakeGenericType(genericArgument);
+            ConstructorInfo[] constructors = constructedListType.GetConstructors();
+
+            Console.WriteLine($"Конструкторы класса List<{genericArgument.Name}>:");
+            foreach (var constructor in constructors)
+            {
+                Console.WriteLine(constructor.ToString());
+            }
+        }
+    }
+}
